8d4b8b8fc0e4d7a8c78c565fa59d35cf2d5ddfbe
Pavel Pavlov <pavelpa@BGSOFEFFL62-2.local>
2021-07-05 12:36:45 +0300

    added first class

---
+++ Example2.java
@@ -0,0 +1,11 @@
+public class Example2 {
+    public static void main(String args[]){
+
+        int num; //declare a variable
+        num = 100; //this assigns the value of 100 to num
+
+        System.out.println("the value of num is " + num);
+
+    }
+    
+}
